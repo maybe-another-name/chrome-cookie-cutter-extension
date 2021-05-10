8eb4dfd9b8edec8c3bd38ea846b74108e4756f73
maybe-another-name <email.yet.another.one@gmail.com>
2021-05-10 14:56:33 -0400

    de-minifying
    filling in some reasonable names for some methods and variables..

--- js/bg.1.js
+++ js/bg.1.js
@@ -1,82 +1,92 @@
-'use strict';
+"use strict";
 (function () {
-	var d;
-	const l = chrome.runtime && chrome.runtime.id, m = chrome.runtime.getURL('manager.html'), g = Date.now(), n = 1631048340000 > g ? !0 : !1, c = function (a) {
-			(n || 1 == a);
-		}, b = function () {
-			var a = chrome.runtime.lastError;
-			a && console.info(a.message);
-			return a;
-		}, h = function (a) {
-			chrome.windows.create({
-				url: a,
-				incognito: !0,
-				type: 'normal',
-				state: 'maximized'
-			}, function (e) {
-				b() && chrome.tabs.create({ url: a }, b);
-			});
-		}, f = function () {
-			chrome.tabs.create({ url: m }, function (a) {
-				b();
-				d = a.id;
-			});
-		}, k = function () {
-			if (d)
-				try {
-					chrome.tabs.update(d, { active: !0 }, function () {
-						chrome.runtime.lastError && f();
-					});
-				} catch (a) {
-					f();
-				}
-			else
-				f();
-		}, p = function (a) {
-			const e = a && a.reason || '';
-			a = a && a.previousVersion || '2.1.0.0';
-		}, q = function () {
-			c(1);
-		}, r = function (a) {
-			chrome.cookies.getAll({}, function (e) {
-				b() ? a(null) : a(e);
-			});
-		}, t = function (a) {
-			a.forEach(a => {
-				chrome.cookies.remove({
-					url: a.url,
-					name: a.name
-				}, function () {
-					b();
-					chrome.cookies.set(a, b);
-				});
-			});
-		}, u = function (a) {
-			a.forEach(a => {
-				chrome.cookies.remove(a, b);
-			});
-		};
-	try {
-		chrome.runtime.onInstalled.addListener(p);
-	} catch (a) {
-		b();
-	}
-	try {
-		chrome.runtime.onStartup.addListener(q);
-	} catch (a) {
-		b();
-	}
-	chrome.runtime.onMessage.addListener((a, b, c) => {
-		if (b && b.id === l) {
-			try {
-				a = JSON.parse(a);
-			} catch (v) {
-				a = {};
-			}
-			1 == a.id ? (d = b && b.tab && b.tab.id || null, c()) : 3 === a.id ? r(c) : 5 === a.id ? (t(a.v || []), c()) : 7 === a.id && (u(a.v || []), c());
-			return !0;
-		}
-		return !1;
-	});
-	chrome.browserAction.onClicked.addListener(k);
-}());
+  var d;
+  const chrome_runtime_info = chrome.runtime && chrome.runtime.id,
+    manger_full_url = chrome.runtime.getURL("manager.html"),
+    now_date = Date.now(),
+    date_check = 1631048340000 > now_date ? !0 : !1,
+    date_check_function = function (a) {
+      date_check || 1 == a;
+    },
+    log_last_error = function () {
+      var a = chrome.runtime.lastError;
+      a && console.info(a.message);
+      return a;
+    },
+    create_tab = function () {
+      chrome.tabs.create({ url: manger_full_url }, function (a) {
+        log_last_error();
+        d = a.id;
+      });
+    },
+    update_or_create_tab = function () {
+      if (d)
+        try {
+          chrome.tabs.update(d, { active: !0 }, function () {
+            chrome.runtime.lastError && create_tab();
+          });
+        } catch (a) {
+          create_tab();
+        }
+      else create_tab();
+    },
+    version_check_callback = function (a) {
+      a = (a && a.previousVersion) || "2.1.0.0";
+    },
+    another_date_check_function = function () {
+      date_check_function(1);
+    },
+    apply_function_to_cookies = function (function_to_apply) {
+      chrome.cookies.getAll({}, function (e) {
+        log_last_error() ? function_to_apply(null) : function_to_apply(e);
+      });
+    },
+    remove_cookies_by_attribute = function (a) {
+      a.forEach((a) => {
+        chrome.cookies.remove(
+          {
+            url: a.url,
+            name: a.name,
+          },
+          function () {
+            log_last_error();
+            chrome.cookies.set(a, log_last_error);
+          }
+        );
+      });
+    },
+    remove_cookies = function (a) {
+      a.forEach((a) => {
+        chrome.cookies.remove(a, log_last_error);
+      });
+    };
+  try {
+    chrome.runtime.onInstalled.addListener(version_check_callback);
+  } catch (a) {
+    log_last_error();
+  }
+  try {
+    chrome.runtime.onStartup.addListener(another_date_check_function);
+  } catch (a) {
+    log_last_error();
+  }
+  chrome.runtime.onMessage.addListener((json_message, chrome_runtime, function_callback) => {
+    if (chrome_runtime && chrome_runtime.id === chrome_runtime_info) {
+      try {
+        json_message = JSON.parse(json_message);
+      } catch (v) {
+        json_message = {};
+      }
+      1 == json_message.id
+        ? ((d = (chrome_runtime && chrome_runtime.tab && chrome_runtime.tab.id) || null), function_callback())
+        : 3 === json_message.id
+        ? apply_function_to_cookies(function_callback)
+        : 5 === json_message.id
+        ? (remove_cookies_by_attribute(json_message.v || []), function_callback())
+        : 7 === json_message.id && (remove_cookies(json_message.v || []), function_callback());
+      return !0;
+    }
+    return !1;
+  });
+  chrome.browserAction.onClicked.addListener(update_or_create_tab);
+})();
